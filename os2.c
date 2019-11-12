828b67eb067758d33f00b82d28821a9db28a4dd3
JunboXu <43670418+JunboXu@users.noreply.github.com>
2019-11-12 16:37:21 +0800

    ls 实现正常打印

---
+++ os2.c
@@ -0,0 +1,283 @@
+#include <stdio.h>
+#include <string.h>
+//全局定义区
+typedef unsigned char byte;
+typedef unsigned short word;
+typedef unsigned int dword;
+//-------------------
+
+//结构体声明与全局变量声明区
+struct Node {
+	char name[11];
+	byte attribute;//属性
+	char reserve[10];//保留字段
+	word time1;
+	word time2;
+	word startSec;//开始扇区
+	dword fileSize;
+};
+
+struct Table {
+	char name[11];
+	byte attribute;
+	word startSec;
+	dword fileSize;
+};
+
+struct Table table[100];
+int t_size = 0;
+
+void preput(char x1[1024], int color);//打印函数
+void input(FILE* fat);
+void printTxtName(char name[11]);
+void ls(FILE* fat12, int begin, char* path, int hasParam);//hasParam =0 -->ls();   1-->ls -l; ;  2-->ls -l 路径
+int  startwith(char x1[64], char x2[64]);
+int findDir(char str[64]);
+void load(char name[64], int begin, FILE* fat12);
+
+int main() {
+	preput("SYSTEM START!",0);
+	preput("\n", 0);
+	FILE* fat12 = fopen("ref.img", "rb"); //打开文件
+	//load("/",19*512,fat12);
+	input(fat12);
+}
+
+void input(FILE* fat12) {
+	char str1[64];
+	char str2[64];
+	char str3[64];
+	while (1){
+		preput("PLEASE INPUT:", 0);
+		scanf("%s", str1);//读指令
+		if (strcmp(str1,"exit")==0)
+		{
+			break;
+		}
+		if (strcmp(str1,"cat")==0)
+		{
+			scanf("%s", str2);//将路径名读到str2中
+
+		}
+		if (strcmp(str1, "ls") == 0) {//如果是ls指令
+			char c = getchar();
+			if (c == '\n') {//如果不带参数，即为ls()指令，从根目录开始输出
+				ls(fat12, 19 * 512, "", 0);
+			}
+			else {//有第二段指令
+				scanf("%s", str2);
+				if (startwith(str2, "-l") || startwith(str2, "-ll"))
+				{
+					char c2 = getchar();
+					if (c2 == '\n')//ls -l()
+					{
+						ls(fat12, 19 * 512, "", 1);
+					}
+					else {
+						scanf("%s", str3);
+						int st = findDir(str3);
+						ls(fat12, 19 * 512, str3, 1);
+					}
+				}
+				else {
+					if (startwith(str2, "/"))
+					{
+						char c3 = getchar();
+						if (c3 == '\n')
+						{//ls + 路径
+							ls(fat12, 19 * 512, str2, 1);
+						}
+						else {
+							if (startwith(str3, "-l") || startwith(str3, "-ll"))
+							{//有第三段输入，且合法
+
+							}
+							else {//非法输入
+								preput("第三段输入非法", 0);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+void ls(FILE* fat12, int begin, char path[64], int hasParam) {
+	struct Node  node0;
+	struct Node* nodeptr = &node0; //初始化节点指针
+	if (hasParam == 0)
+	{
+		preput(path, 0);
+		preput("/:", 0);
+		preput("\n", 0);
+		if (strlen(path) != 0)
+		{
+			preput(". .. ", 0);
+		}
+		for (int i = 0; i < 15; i++)//先一个循环打印出直属文件夹和文件
+		{
+			fseek(fat12, begin + i * 32, 0);
+			fread(nodeptr, 1, 32, fat12);
+			if ((nodeptr->name[0] >= 'A' && nodeptr->name[0] <= 'Z') || (nodeptr->name[0] >= 'a' && nodeptr->name[0] <= 'z') || (nodeptr->name[0] >= '0' && nodeptr->name[0] <= '9'))
+			{
+			}
+			else { continue; }
+			if (nodeptr->attribute == 0x10 || nodeptr->attribute == 0x00)
+			{
+				if (nodeptr->attribute == 0x00)//文本文件
+				{
+					printTxtName(nodeptr->name);
+					preput(" ", 0);
+				}
+				if (nodeptr->attribute == 0x10)//文件夹
+				{
+					int nameLen = 0;
+					char temp[11];
+					for (int i = 0; i < 11; i++)
+					{
+						if (nodeptr->name[i] == '\0' || nodeptr->name[i] == ' ') {
+							temp[i] = '\0';
+							break;
+						}
+						else {
+							temp[i] = nodeptr->name[i];
+							nameLen++;
+						}
+					}
+					preput(temp, 0);
+					preput(" ", 0);
+				}
+			}
+		}
+		preput("\n", 0);
+		for (int i = 0; i < 15; i++)//再一个循环顺序判断直属是否为文件夹，是文件夹就进文件夹，打印
+		{
+			fseek(fat12, begin + i * 32, 0);
+			fread(nodeptr, 1, 32, fat12);
+			if ((nodeptr->name[0] >= 'A' && nodeptr->name[0] <= 'Z') || (nodeptr->name[0] >= 'a' && nodeptr->name[0] <= 'z') || (nodeptr->name[0] >= '0' && nodeptr->name[0] <= '9'))
+			{
+			}
+			else { continue; }
+			char temppath[64];
+			char nowName[64];
+			int tempbegin;
+			if (nodeptr->attribute == 0x10)//如果是文件夹，则将文件夹名称写入当前路径
+			{
+				char* gang = "/";
+				if (strlen(path) == 0 || path == "/")
+				{
+					path = "/";
+				}
+				else {
+					strcat(path, gang);
+				}
+				for (int i = 0; i < 64; i++)
+				{
+					temppath[i] = path[i];
+					if (path[i] == '\0' || path[i] == ' ')
+					{
+						temppath[i] = '\0';
+						break;
+					}
+				}//将当前路径存到temppath中
+				for (int i = 0; i < 64; i++)
+				{
+					nowName[i] = nodeptr->name[i];
+					if (nodeptr->name[i] == '\0' || nodeptr->name[i] == ' ')
+					{
+						nowName[i] = '\0';
+						break;
+					}
+				}
+				strcat(temppath, nowName);//写入当前路径
+
+				tempbegin = (nodeptr->startSec + 31) * 512;//跳转到该文件夹指向的簇
+
+				ls(fat12, tempbegin, temppath, 0);//递归ls
+			}
+		}
+	}
+	//--------ls -l +路径
+	if (hasParam)
+	{
+
+	}
+
+	return;
+}
+
+/*
+ * load 函数。
+ * 递归地扫描该文件夹下的文件，若为txt 文件，则存进来。若为文件夹，则先存，再递归地调用此方法。
+ * 假设一个文件夹下存放不超过15个文件
+ *
+ */
+
+void load(char path[64], int begin, FILE* fat12) {
+	//-----
+	struct Node  node0;
+	struct Node* nodeptr = &node0; //初始化节点指针
+	char temppath[64];
+	char nowName[64];
+	int tempbegin;
+	for (int i = 0; i < 15; i++) {//第一遍循环存放当前目录下的文件进表
+		fseek(fat12, begin + i * 32, 0);
+		fread(nodeptr, 1, 32, fat12);
+		if (nodeptr->attribute == 0x10 || nodeptr->attribute == 0x20) {//文件类型是文件夹0x10,文件0x20
+			if ((nodeptr->name[0] >= 'A' && nodeptr->name[0] <= 'Z') || (nodeptr->name[0] >= 'a' && nodeptr->name[0] <= 'z') || (nodeptr->name[0] >= '0' && nodeptr->name[0] <= '9'))
+			{
+			}
+			else { continue; }
+
+			if (nodeptr->attribute == 0x20)//如果是文件夹，则将文件夹名称写入当前路径
+			{
+				table[t_size].attribute = nodeptr->attribute;
+			
+			}
+		}
+	}
+	return;
+}
+
+int findDir(char str[64]) {
+	
+}
+void printTxtName(char name[11]) {
+	char temp[11];
+	for (int i = 0; i < 8; i++)
+	{
+		if (name[i] == '\0'||name[i]==' ') {
+			temp[i] = '\0';
+			break;
+		}
+		else {
+			temp[i] = name[i];
+		}
+	}
+	preput(temp, 0);
+	preput(".txt", 1);
+	preput(" ", 0);
+}
+int  startwith(char x1[64], char x2[64]) {
+	int len = 0;
+	while (x2[len] != '\0') {
+		len++;
+	}
+	int res = 1;
+	for (int i = 0; i < len; i++) {
+		if (x1[i] != x2[i]) {
+			res = 0;
+			break;
+		}
+	}
+	return res;
+}
+
+void preput(char x1[1024], int color) {
+	int len = 0;
+	char put[64];
+	while (x1[len] != '\0') { len++; }
+	strcpy(put, x1);
+	printf("%s", put);
+	//output(put, color, len);
+}
